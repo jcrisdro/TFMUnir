d730d10246e9670e63e38cff839b630bb0bcfc8f
jdfinkargo <joseph.diaz@finkargo.com>
2024-05-26 00:43:12 -0500

    Add whisper to process ETL

--- project/services/etl.py
+++ project/services/etl.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import whisper
 
 from moviepy.editor import VideoFileClip
 
@@ -12,32 +13,47 @@ class ETL:
     def split(self, root_path: str = None, file_path: str = None, folder_name: str = None):
         try:
             os.mkdir(f"{root_path}/resources/uploads/{folder_name}")
+            os.mkdir(f"{root_path}/resources/uploads/{folder_name}/frames")
         except Exception as e:
             print(f"Exception: {e}")
 
         try:
             movie = VideoFileClip(f"{root_path}/{file_path}")
             audio = movie.audio
-            audio_path = f"/resources/uploads/{folder_name}/audio.mp3"
+            audio_path = f"/resources/uploads/{folder_name}/{folder_name}.mp3"
             audio.write_audiofile(f"{root_path}/{audio_path}")
         except Exception as e:
             print(f"Exception: {e}")
         finally:
             movie.close()
             audio.close()
-        print(f"getting audio from video... {audio_path}")
+            print(f"Finish audio from video... {audio_path}")
 
         try:
             capture = cv2.VideoCapture(f"{root_path}/{file_path}")
-            cont = 0
+            frames = 0
             while(capture.isOpened()):
                 ret, frame = capture.read()
                 if ret is True:
-                    cont += 1
-                    cv2.imwrite(f"{root_path}/resources/uploads/{folder_name}/frame_{cont}.jpg", frame)
+                    frames += 1
+                    cv2.imwrite(f"{root_path}/resources/uploads/{folder_name}/frames/{frames}.jpg", frame)
                 else:
                     break
         except Exception as e:
             print(f"Exception: {e}")
-        print(f"getting frames from video... {file_path}")
-        return cont, audio_path
+        finally:
+            print("Finish getting frames...")
+
+        text = {}
+        try:
+            whisper_model = whisper.load_model("base")
+            text = whisper_model.transcribe(f"{root_path}/resources/uploads/{folder_name}/{folder_name}.mp3")
+            with open(f"{root_path}/resources/uploads/{folder_name}/{folder_name}.txt", "w") as file:
+                file.write(text.get('text', 'No transcription found'))
+        except Exception as e:
+            print(f"Exception: {e}")
+        finally:
+            print("Finish transcrypting video...")
+
+        response = {'frames': frames, 'key': folder_name, 'language': text.get('language', 'No language found')}
+        return response
