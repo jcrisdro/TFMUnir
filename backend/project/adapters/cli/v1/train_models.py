6e00e7f08757db08232bb763a4f59b8d48898efc
jdfinkargo <joseph.diaz@finkargo.com>
2024-07-06 10:20:46 -0500

    Separar carpetas mobile de backend & Adicionar imagenes de referencia readme

---
+++ backend/project/adapters/cli/v1/train_models.py
@@ -0,0 +1,13 @@
+class TrainModelsCliAdapter:
+    """ training model adapter """
+
+    def __init__(self) -> None:
+        pass
+
+    def hamodel(self):
+        """ hearing aid model """
+        print("hamodel")
+
+    def vsmodel(self):
+        """ visual support model """
+        print("vsmodel")
